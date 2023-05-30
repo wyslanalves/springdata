6522bc488395e87e56ea9ff9152f67893ea83838
wyslan alves <wyslandasilvaalves@gmail.com>
2023-05-29 21:49:00 -0300

    using jpql

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -2,29 +2,40 @@ package br.com.alura.spring.data;
 
 import java.util.Scanner;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosSevice;
 
+@EnableJpaRepositories
 @SpringBootApplication
-public class SpringDataApplication implements CommandLineRunner{
-	
-	private final CrudCargoService cargoService;
-	
+public class SpringDataApplication implements CommandLineRunner {
+
 	private Boolean system = true;
 
-	@Autowired
-	private CargoRepository cargoRepository;
+	private final CrudCargoService cargoService;
+
+	private final CrudFuncionarioService funcionarioService;
+
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
 	
-	public SpringDataApplication(CrudCargoService cargoService) {
+	private final RelatoriosSevice relatoriosSevice;
+
+	public SpringDataApplication(CrudCargoService cargoService,
+			CrudFuncionarioService funcionarioService, 
+			CrudUnidadeTrabalhoService unidadeTrabalhoService,
+			RelatoriosSevice relatoriosSevice) {
 		this.cargoService = cargoService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosSevice = relatoriosSevice;
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
@@ -32,24 +43,35 @@ public class SpringDataApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		while (system) {
-			System.out.println("Qual acao voce quer executar");
+			System.out.println("Qual função deseja executar?");
 			System.out.println("0 - Sair");
-			System.out.println("1 - Cargo");
+			System.out.println("1 - Funcionario");
+			System.out.println("2 - Cargo");
+			System.out.println("3 - Unidade");
+			System.out.println("4 - Relatorios");
 			
-			int action = scanner.nextInt();
-			if(action == 1) {
-				cargoService.inicial(scanner);
-			}else {
-				system = false;
+			Integer function = scanner.nextInt();
+
+			switch (function) {
+				case 1:
+					cargoService.inicial(scanner);
+					break;
+				case 2:
+					funcionarioService.inicial(scanner);
+					break;
+				case 3:
+					unidadeTrabalhoService.inicial(scanner);
+					break;
+				case 4:
+					relatoriosSevice.inicial(scanner);
+					break;
+				default:
+					System.out.println("Finalizando");
+					system = false;
+					break;
 			}
 		}
-		
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESEVOLVEDOR DE SOFTWARE");
-		cargoRepository.save(cargo);
-		
 	}
-
 }
---
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosSevice.java
@@ -0,0 +1,76 @@
+package br.com.alura.spring.data.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.repository.FuncionarioRepository;
+
+@Service
+public class RelatoriosSevice {
+
+	private Boolean system = true;
+	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+ 
+// @formatter:on
+
+	
+	private FuncionarioRepository funcionarioRepository;
+	
+	public RelatoriosSevice(FuncionarioRepository funcionarioRepository) {
+		this.funcionarioRepository = funcionarioRepository;
+	}
+
+	public void inicial(Scanner scanner) {
+
+		while (system) {
+			System.out.println("Qual ação de cargo deseja executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Busca Funcionario Nome");
+			System.out.println("2 - Busca Funcionario Nome, Data Contratação e Salario Maior");
+			
+
+			int action = scanner.nextInt();
+
+			switch (action) {
+			case 1:
+				buscaFuncionarioNome(scanner);
+				break;
+			case 2:
+				buscaFuncionarioNomeSalarioMaiorData(scanner);
+				break;
+			default:
+				system = false;
+				break;
+			}
+		}
+	}
+	
+	private void buscaFuncionarioNome(Scanner scanner) {
+		System.out.println("Qual Nome Deseja Pesquisar");
+		String nome = scanner.next();
+		List<Funcionario> list = funcionarioRepository.findByNome(nome);
+		list.forEach(System.out::println);
+	}
+	
+	private void buscaFuncionarioNomeSalarioMaiorData(Scanner scanner) {
+		System.out.println("Qual Nome Deseja Pesquisar?");
+		String nome = scanner.next();
+		
+		System.out.println("Qual Data de contratação Deseja Pesquisar?");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		System.out.println("Qual Salario Deseja Pesquisar?");
+		Double salario = scanner.nextDouble();
+		
+		List<Funcionario> list = funcionarioRepository.findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
+		list.forEach(System.out::println);
+	}
+}
